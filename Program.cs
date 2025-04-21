d19c01f7f2fd37a5ad4deed636a5676542acb585
Pedro Guilherme <105387878+pedrog09@users.noreply.github.com>
2025-04-21 16:45:30 -0300

    .

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿
+using System.Security.AccessControl;
+
 namespace TextEditor
 {
     public class Program()
     {
         public static void Main(string[] args)
         {
-
+            Menu();
         }
 
         static void Menu()
@@ -22,18 +24,59 @@ namespace TextEditor
             {
                 case 0: System.Environment.Exit(0); break;
                 case 1: Abrir(); break; 
-                case 2: Criar(); break;
+                case 2: Editar(); break;
                 default: Menu(); break;
             }
             }
 
-            static void Criar()
-            {
-                
-            }
             static void Abrir()
             {
-                Console.WriteLine("1");
+                Console.Clear();
+                Console.WriteLine("Qual o caminho para abrir o arquivo?");
+                string path = Console.ReadLine();
+
+                using (var file = new StreamReader(path))
+                {
+                    string text = file.ReadToEnd();
+                    Console.WriteLine(text);
+                }
+
+                Console.WriteLine("");
+                Console.ReadLine();
+                Menu();
+            }
+
+            static void Editar()
+            {
+                Console.Clear();
+                Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+                Console.WriteLine("--------------------------------------");
+                string text = "";
+
+                do
+                {
+                    text += Console.ReadLine();
+                    text += Environment.NewLine;
+                }
+                while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+                Salvar(text);
+            }
+
+            static void Salvar(string text)
+            {
+                Console.Clear();
+                Console.WriteLine("Qual caminho para salvar o arquivo?");
+                var path = Console.ReadLine();
+
+                using (var file = new StreamWriter(path))
+                {
+                    file.Write(text);
+                } 
+
+                Console.WriteLine($"O arquivo foi salvo com sucesso em:{path}");
+                Console.ReadLine();
+                Menu();
             }
     }
 
